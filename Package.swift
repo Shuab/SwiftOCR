c006a6363c5d8a000b7457d87027571fb7e6db87
Joshua Alexander <shuabeenz@gmail.com>
2020-07-15 02:09:22 -0400

    Changed Package target name

--- Package.swift
+++ Package.swift
@@ -11,7 +11,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftOCR",
-            targets: ["SwiftOCR_Mac"]),
+            targets: ["SwiftOCR Mac"]),
     ],
     targets: [
         .target(name: "SwiftOCR Mac")
